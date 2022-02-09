2d2beaf4ca247d018a3326e932b0fa6143124de2
ManasAtabaev <manas.atabaev@gmail.com>
2022-02-09 15:06:10 +0800

    move 's' to template

--- src/components/ReposList.vue
+++ src/components/ReposList.vue
@@ -69,7 +69,7 @@
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-center">
                   <div class="text-sm text-gray-900">
-                    {{ estimatedDownloadTimeHelper.get(item.size) }}
+                    {{ estimatedDownloadTimeHelper.get(item.size) }}s
                   </div>
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap">
--- src/helpers/estimatedDownloadTimeHelper.ts
+++ src/helpers/estimatedDownloadTimeHelper.ts
@@ -2,7 +2,7 @@ const get = (size: number) => {
   const date = new Date();
   const time = date.getHours();
   const bandwidth = (10000 * (1 - Math.E * ((time * -1) / 3))) / 1000;
-  return (size / bandwidth).toFixed(2) + 's';
+  return (size / bandwidth).toFixed(2);
 };
 
 export const estimatedDownloadTimeHelper = {
