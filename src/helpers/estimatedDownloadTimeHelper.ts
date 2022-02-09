2432160ddb38193cd6b840fab7ec857df94ec6bb
ManasAtabaev <manas.atabaev@gmail.com>
2022-02-09 15:11:52 +0800

    fix estimate time helper formula

--- src/helpers/estimatedDownloadTimeHelper.ts
+++ src/helpers/estimatedDownloadTimeHelper.ts
@@ -1,7 +1,7 @@
 const get = (size: number) => {
   const date = new Date();
   const time = date.getHours();
-  const bandwidth = (10000 * (1 - Math.E * ((time * -1) / 3))) / 1000;
+  const bandwidth = 10000 * (1 - Math.E * ((time * -1) / 3));
   return (size / bandwidth).toFixed(2);
 };
 
