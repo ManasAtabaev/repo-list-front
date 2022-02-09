3af2205548692b932f1bb7fa0a07fa473e8b0571
ManasAtabaev <manas.atabaev@gmail.com>
2022-02-09 11:49:24 +0800

    Add ReposList component test

---
+++ tests/unit/ReposList.spec.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import ReposList from '@/components/ReposList.vue';
+import { Repository } from '@/models';
+import { setActivePinia, createPinia } from 'pinia';
+
+describe('ReposList.vue', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it('renders repos list when passed', () => {
+    const data = [
+      new Repository({
+        id: 38674532,
+        node_id: 'MDEwOlJlcG9zaXRvcnkzODY3NDUzMg==',
+        name: 'Tetris',
+        full_name: 'sandywalker/Tetris',
+        html_url: 'https://github.com/sandywalker/Tetris',
+        description: 'Html5 Tetris game',
+        owner: {
+          login: 'sandywalker',
+          id: 8475219,
+          html_url: 'https://github.com/sandywalker',
+        },
+        size: 166,
+        estDownloadTime: () => 2,
+      }),
+    ];
+    const wrapper = shallowMount(ReposList, {
+      props: { data },
+    });
+    expect(wrapper.text()).to.include(data[0].full_name);
+  });
+});
