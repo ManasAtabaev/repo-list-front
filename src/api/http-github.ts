6c989ffe1f565ffe8fdb094bab87978e28249030
ManasAtabaev <manas.atabaev@gmail.com>
2022-02-09 14:41:11 +0800

    Add ratelimit plugin

--- src/api/http-github.ts
+++ src/api/http-github.ts
@@ -1,8 +1,13 @@
 import axios, { AxiosInstance } from 'axios';
-const apiClient: AxiosInstance = axios.create({
-  baseURL: 'https://api.github.com',
-  headers: {
-    'Content-type': 'application/json',
-  },
-});
+import rateLimit from 'axios-rate-limit';
+
+const apiClient: AxiosInstance = rateLimit(
+  axios.create({
+    baseURL: 'https://api.github.com',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  }),
+  { maxRequests: 10, perMilliseconds: 60000, maxRPS: 1 }
+);
 export default apiClient;
