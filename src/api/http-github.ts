02c6687a62b8c7d5a8c3f7f0ae3ec7e01495c64a
ManasAtabaev <manas.atabaev@gmail.com>
2022-02-09 15:36:04 +0800

    Add comments to http-connection for github

--- src/api/http-github.ts
+++ src/api/http-github.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 import rateLimit from 'axios-rate-limit';
 
+//Best way need read rate limits from response header, github already returning all required data via x-ratelimit
 const apiClient: AxiosInstance = rateLimit(
   axios.create({
     baseURL: 'https://api.github.com',
